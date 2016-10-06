ecc3bf0124ce878ba309421b6242ed99b49f415e
bpoloney <brendan.poloney@gmail.com>
2016-10-05 20:06:37 -0400

    UI tweaks

---
+++ startup cities multiplayer game/assets/scripts/UIScripts/LeaderboardToggle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaderboardToggle : MonoBehaviour {
+
+	public GameObject g;
+
+	public void ToggleLeaderboard() {
+		bool b = g.activeInHierarchy;
+		g.SetActive (!b);
+	}
+}
