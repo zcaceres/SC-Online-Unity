1dfa5e995a1e9a4821dd69ba8c68e9e09a42755a
bpoloney <brendan.poloney@gmail.com>
2016-10-27 17:02:18 -0400

    terrain reseting
    terrain changes will no longer persist after closing the game

---
+++ startup cities multiplayer game/assets/scripts/Contruction/TerrainReset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainReset : MonoBehaviour {
+
+	public float[,] originalHeights;
+	Terrain terrain;
+	// Use this for initialization
+	void Start () {
+		terrain = GetComponent<Terrain> ();
+		originalHeights = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+	}
+
+	/// <summary>
+	/// When the terrain is destroyed, reset the values
+	/// </summary>
+	void OnDestroy()
+	{
+		terrain.terrainData.SetHeights(0, 0, originalHeights);
+	}
+}
